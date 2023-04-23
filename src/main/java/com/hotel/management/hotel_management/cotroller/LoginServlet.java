c59b7d653d407f4109e605c810446b5553c1b910
mohammed-eddarif <mohammed.eddari06@gmail.com>
2023-04-23 12:41:04 +0100

    adding servlet api so that tomcat can read classes as "httpServlet ..."

--- src/main/java/com/hotel/management/hotel_management/cotroller/LoginServlet.java
+++ src/main/java/com/hotel/management/hotel_management/cotroller/LoginServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+
 import java.io.IOException;
 import java.util.List;
 
--- src/main/webapp/views/maitreOuvrage/maitre_ouvrage_home.jsp
+++ src/main/webapp/views/maitreOuvrage/maitre_ouvrage_home.jsp
@@ -12,5 +12,7 @@
 </head>
 <body>
 
+<h1>hello from maitre d'ouvrage</h1>
+
 </body>
 </html>
